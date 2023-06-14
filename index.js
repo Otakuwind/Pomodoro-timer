4881c8f7a75a5acbe971eaea869b52238584a4ca
Edgar Nwajei <edgarlinked@gmail.com>
2023-06-14 15:29:25 +0100

    linked files

---
+++ index.js
@@ -0,0 +1,17 @@
+const minElem = document.querySelector("#minutes");
+const secElem = document.querySelector("#seconds");
+const setting= document.querySelector("#setting");
+
+let toggleSettings = false;
+
+setting.onclick = function()
+{
+    if (toggleSettings) {
+toggleSettings = true;
+minElem.contentEditable = true;
+minElem.style
+
+
+    }
+
+}
